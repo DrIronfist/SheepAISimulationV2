fe311456fa6c9b93662244b74ac783dac7a231ea
DrIronfist <reetsinha2006@gmail.com>
2023-07-23 21:27:38 -0500

    sheep changes

--- src/Sheep.java
+++ src/Sheep.java
@@ -1,7 +1,25 @@
 
 public class Sheep {
 
+   //constants
+   public static double starveRate, starvePenalty, thirstRate, thirstPenalty = 0.01;
+   public static double maxSpeed = 1;
+   public static int maxLifeSpan = 1000;
+   public static int matingReq = 180;
+
+   //sheep variables
+   public double hunger = 1;
+   public double thirst = 1;
+   public int age = 0;
+
+   
+
+
    public  Sheep(){
 
    }
+
+
+
+
 }
